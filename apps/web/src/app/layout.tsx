81d3e7153409e71afb11584b50a04f10bc3ecaa9
Pontus Abrahamsson <pontus@lostisland.co>
2024-12-23 11:24:36 +0100

    Add OP

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,5 +1,7 @@
+import { OpenPanelComponent } from "@openpanel/nextjs";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,6 +29,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <OpenPanelComponent
+          clientId={process.env.NEXT_PUBLIC_OPEN_PANEL_CLIENT_ID!}
+          clientSecret={process.env.OPEN_PANEL_CLIENT_SECRET!}
+          trackScreenViews={true}
+        />
         {children}
       </body>
     </html>
