638059272aac20c8dc59bfe547e9bbc809a673f3
Pontus Abrahamsson <pontus@lostisland.co>
2024-12-29 15:50:04 +0100

    Add HN

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -36,6 +36,35 @@ export default function RootLayout({
           disabled={process.env.NODE_ENV !== "production"}
         />
         {children}
+
+        <div className="fixed bottom-4 right-4 flex items-center gap-2">
+          <a
+            href="https://dub.sh/lng"
+            target="_blank"
+            rel="noreferrer"
+            className="flex items-center gap-2"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="none"
+            >
+              <g clipPath="url(#a)">
+                <path
+                  fill="#F60"
+                  d="M0 0v16h16V0H0Zm8.7 9.225v3.925H7.275V9.225L3.775 2.3h1.65L8 7.525 10.65 2.3h1.55L8.7 9.225Z"
+                />
+              </g>
+              <defs>
+                <clipPath id="a">
+                  <path fill="#fff" d="M0 0h16v16H0z" />
+                </clipPath>
+              </defs>
+            </svg>
+            <span>Live on Hacker News</span>
+          </a>
+        </div>
       </body>
     </html>
   );
